888002415f1f73e796df248c833762b4de689a64
droidicus <droidicus@dr.com>
2022-05-21 14:25:06 -0400

    first iteration

---
+++ calc_pay.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+""" This script parses the bank CSV file, calculated the commission per employee, and
+    output the data to the console for human consumption
+"""
+import argparse
+from datetime import datetime, time, timedelta
+
+import pandas as pd
+import seaborn as sns
+from dateutil.relativedelta import FR, relativedelta
+
+# CONSTANTS
+COMMISSION_RATE = 0.3
+FILENAME = "sotw.csv"
+
+# The main thing
+def main(filename):
+    # Read in the data
+    df = pd.read_csv(filename)
+
+    # Decode the 'direction' field
+    df["change"] = df["change"] * (1 - 2 * (df["direction"] == "outgoing"))
+
+    # lol... cumsum...
+    df["balance"] = df.loc[::-1, "change"].cumsum()[::-1]
+
+    # Decode the timestamp
+    df["datetime"] = pd.to_datetime(df["timestamp"])
+
+    # Someone doesn't sanitize their inputs...
+    df["requestor"] = df["requestor"].str.replace("\t", " ")
+    df["requestor"] = df["requestor"].str.replace("\n", " ")
+
+    # Index on the datetime
+    df_dtindex = df.set_index("datetime")
+
+    # Calculate the last friday of a full week of data
+    last_friday = datetime.now().date() + relativedelta(weekday=FR(-1))
+    last_friday = datetime.combine(last_friday, time(0))
+
+    # Slice the week of transactions
+    df_week = df_dtindex.loc[last_friday : last_friday - timedelta(weeks=1)]
+
+    # Select the purchases
+    df_purchases = df_week[df_week["type"] == "purchase"]
+
+    # Group by person charging, calculate commissions, print it out
+    commissions = (
+        df_purchases.groupby(["requestor"]).sum()["change"] * COMMISSION_RATE
+    ).round()
+    print(commissions)
+
+
+# Are you prepared?
+if __name__ == "__main__":
+    # Performing diagnostic
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", type=str, default=FILENAME)
+    args = parser.parse_args()
+
+    # Affirm
+    main(args.file)
